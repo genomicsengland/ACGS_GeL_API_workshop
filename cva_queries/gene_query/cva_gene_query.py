ff5f7e563a8696246096215900d58e36ccdaf41c
Chris Boustred <christopher.boustred@genomicsengland.co.uk>
2023-02-07 17:02:56 +0000

    chore: add some more logging and descriptions

--- cva_queries/gene_query/cva_gene_query.py
+++ cva_queries/gene_query/cva_gene_query.py
@@ -5,12 +5,21 @@ from datetime import datetime
 import csv
 import argparse
 import getpass
+import logging
 
 """
 
-Script that queries CVA for a gene symbol and checks if that gene contains any tier1 or tier2 variants
+Script queries CVA for an Ensembl gene ID and checks if the gene contains any tier1 or tier2 variants.
+
+Produces a .csv output list the cases that have Tier 1 or Tier 2 variants in that gene
+
+Tested on SOD1: ENSG00000142168
+
+python cva_gene_query.py -g ENSG00000142168 -u email@nhs.net -o ~/Desktop -p
 
 """
+
+HOST = "https://cva.genomicsengland.nhs.uk/"
 
 
 def parse_arguments():
@@ -46,21 +55,21 @@ def parse_arguments():
 
 
 def check_response(case_variants):
+    """Helper function that checks the response from CVA is looking OK."""
 
-    if len(case_variants["response"]) == 1:
+    if "response" in case_variants.keys() and len(case_variants["response"]) == 1:
         return True
     else:
-        print("error, more / less variants returned than expected!!")
+        print("Error, no API response OR more / less variants returned than expected!!")
         return False
 
 
 def parse_case_variants(case_id, case_variants, gene_symbol):
-    """
+    """Filters CVA API response to only variants in a specific gene that are tier 1 or tier 2.
 
-    :param case_id:
-    :param case_variants:
-    :param gene_symbol:
-    :return:
+    case_id format = 123-1
+    case_variants = response from f"{HOST}cva/api/0/cases/{str(case['identifier'])}/{str(case['version'])}/variants"
+    gene_symbol = Ensembl ID
     """
     # create CVA url for the case
     cva_case_url = f"{HOST}case/{case_id}"
@@ -82,20 +91,42 @@ def parse_case_variants(case_id, case_variants, gene_symbol):
     return gene_variants
 
 
-def get_case_variants(case_list):
+def get_case_variants(cva_client, gene_symbol):
+
+    """Get all the cases that have variants in the gene symbol specified then checks if variants are tier 1 or tier 2
+
+    Uses the "parse_case_variants()" function to check the tier 1 and tier 2 variants in a case
+
+    :param cva_client: authenicated instance of CvaClient()
+    :param gene_symbol: ENSG gene ID
+    :return:
+    """
     output = []
+
+    cases_client = cva_client.cases()
+    # get a list of cases that have a variant in specified gene symbol
+    case_list = list(cases_client.get_cases(genes=[gene_symbol]))
+
     total_cases = len(case_list)
     counter = 0
-    print(total_cases, 'cases found in CVA')
+    print(total_cases, "cases found in CVA")
+    logging.info(total_cases, "cases found in CVA")
     for case in case_list:
         counter += 1
         case_id = "-".join([str(case["identifier"]), str(case["version"])])
-        print(f'Checking case {str(counter)} ({case_id}) for tier 1 and tier 2 variants...')
+        print(
+            f"Checking case {str(counter)} ({case_id}) for tier 1 and tier 2 variants..."
+        )
+        logging.info(
+            f"Checking case {str(counter)} ({case_id}) for tier 1 and tier 2 variants..."
+        )
         # get all the variants in a specific case
-        new_url = f"{HOST}cva/api/0/cases/{str(case['identifier'])}/{str(case['version'])}/variants"
-        x = requests.get(url=new_url, headers={"Authorization": cva._token}).json()
+        cva_case_variants_url = f"{HOST}cva/api/0/cases/{str(case['identifier'])}/{str(case['version'])}/variants"
+        cva_case_variant_data = requests.get(
+            url=cva_case_variants_url, headers={"Authorization": cva_client._token}
+        ).json()
         case_gene_variants = parse_case_variants(
-            case_id=case_id, case_variants=x, gene_symbol=gene_symbol
+            case_id=case_id, case_variants=cva_case_variant_data, gene_symbol=gene_symbol
         )
         if len(case_gene_variants) != 0:
             row = [
@@ -109,6 +140,15 @@ def get_case_variants(case_list):
 
 def write_output_file(case_variant_list, output_file_name):
 
+    """From the cases identified write a comma sep file that includes link to case in CVA
+
+    NOTE the cva case status is the overall case status and not always relevant to the gene / variant identified
+
+    :param case_variant_list:
+    :param output_file_name:
+    :return:
+    """
+
     with open(output_file_name, "w") as f_handle:
         line_writer = csv.writer(f_handle, delimiter=",")
         header = [
@@ -117,7 +157,7 @@ def write_output_file(case_variant_list, output_file_name):
             "cva_status",
             "variant",
             "highest_tier",
-            "#case_link",
+            "cva_case_link",
         ]
         line_writer.writerow(header)
         for case in case_variant_list:
@@ -126,27 +166,29 @@ def write_output_file(case_variant_list, output_file_name):
     return output_file_name
 
 
-if __name__ == "__main__":
+def main():
+    """
 
+    :return:
+    """
     date = datetime.now().strftime("%Y_%m_%d-%I-%M-%S_%p")
-    HOST = "https://cva.genomicsengland.nhs.uk/"
     args = parse_arguments()
 
     gene_symbol = args.gene
-    # password = ""
 
     if args.password:
         password = getpass.getpass()
 
     cva = CvaClient(url_base=HOST, user=args.username, password=password)
-
-    cases_client = cva.cases()
-    # get a list of cases that have a variant in specified gene symbol
-    case_list = list(cases_client.get_cases(genes=[gene_symbol]))
-    case_variants = get_case_variants(case_list=case_list)
+    case_variants = get_case_variants(cva_client=cva, gene_symbol=gene_symbol)
     f_name = f"cva_{gene_symbol}_{date}.csv"
     output_file_name = os.path.expanduser(os.path.join(args.output_dir, f_name))
     output = write_output_file(
         case_variant_list=case_variants, output_file_name=output_file_name
     )
-    print(output)
+    return output
+
+
+if __name__ == "__main__":
+
+    main()
